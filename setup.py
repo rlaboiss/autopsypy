a00e7d8941452af8ec841eab7ad0f33691e63809
Rafael Laboissière <rafael@laboissiere.net>
2023-09-19 16:08:30 +0200

    Bump version

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ from setuptools import setup
 
 setup(
     name="autopsypy",
-    version="0.0.4",
+    version="0.0.5",
     description="Automate PsychoPy experiments",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
